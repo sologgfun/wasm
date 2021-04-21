cfd0eb6ad8b4135d5a20c165d7c8f839d00c95da
wangjianfeng <wangjianfeng@cmss.chinamobile.com>
2021-04-21 11:12:43 +0800

    wasm

--- demo-wasm/test-node/index.js
+++ demo-wasm/test-node/index.js
@@ -1,5 +1,8 @@
 var md5 = require('md5');
-let wasm = require("../pkg/hello_wasm")
+//nodejs 直接调用
+let wasm = require("./node-pkg/hello_wasm")
+//打包rust调用
+//let wasm = require("../Cargo.toml")
 function randomString(e) {
     e = e || 32;
     var t = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678',
@@ -12,13 +15,35 @@ function randomString(e) {
 (async _ => {
     const module = await wasm;         //引入wasm对象
     const str = randomString(999999);  //生成999999位随机字符串
-
     //打印js生成的md5及所需时间
-    console.time('js');
-    console.log('%cjsMd5:' + md5(str), 'color:blue');
-    console.timeEnd('js');
+    var begin=Date.now();
+    console.log('jsMd5:' + md5(str), 'color:blue');
+    var end= Date.now();
+    var timeSpent=(end-begin)/1000+"secs";;
+    console.log(`js:${timeSpent}`);
     //打印wasm生成的md5及所需时间
-    console.time('wasm');
-    console.log('%cwasmMd5:' + module.digest(str), 'color:green');
-    console.timeEnd('wasm');
+    begin=Date.now();
+    console.log('wasmMd5:' + module.digest(str), 'color:green');
+    end= Date.now();
+    timeSpent=(end-begin)/1000+"secs";;
+    console.log(`wasm:${timeSpent}`);
 })();
+
+// module.exports = {
+//     handler: async (event, context) => {
+//         const module = await wasm;         //引入wasm对象
+//         const str = randomString(999999);  //生成999999位随机字符串
+//         //打印js生成的md5及所需时间
+//         var begin=Date.now();
+//         console.log('jsMd5:' + md5(str), 'color:blue');
+//         var end= Date.now();
+//         var timeSpent=(end-begin)/1000+"secs";;
+//         console.log(`js:${timeSpent}`);
+//         //打印wasm生成的md5及所需时间
+//         begin=Date.now();
+//         console.log('wasmMd5:' + module.digest(str), 'color:green');
+//         end= Date.now();
+//         timeSpent=(end-begin)/1000+"secs";;
+//         console.log(`wasm:${timeSpent}`);
+//     }
+//   }
